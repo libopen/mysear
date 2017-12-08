ea0ac34c59db52f35da71356d0e832b487e726fa
libopen <libin_chn@msn.com>
2017-12-08 12:09:52 +0800

    add sh8 tmacd

--- STS.py
+++ STS.py
@@ -36,4 +36,38 @@ def getwposmacd(sn='ss123456'):
         return None
 
 
+def gettmacd(sn='ss123456'):
+    imp.reload(STTB)
+    s=STTB.STDTB(sn)
+    exdb=(s.getexdb()[['date','tmacd']]
+           .set_index(['date'])
+           .loc['2015-1-1':]
+           )
+    return exdb
+
+def getdtmacd(sn='ss123456'):
+    imp.reload(STTB)
+    s=STTB.STDTB(sn)
+    try:
+        exdb=(s.getexdb()[['date','tmacd']]
+               .set_index(['date'])
+               .loc['2015-1-1':]
+               .assign(sn=lambda x:sn)
+               )
+        return exdb
+    except:
+        return None
+
+def getwtmacd(sn='ss123456'):
+    imp.reload(STTB)
+    s=STTB.STWTB(sn)
+    try:
+        exdb=(s.getexdb()[['date','tmacd']]
+               .set_index(['date'])
+               .loc['2015-1-1':]
+               .assign(sn=lambda x:sn)
+               )
+        return exdb
+    except:
+        return None
     
--- ists.py
+++ ists.py
@@ -16,14 +16,14 @@ def getposdf(sn='ss123456'):
         df2=STS.getposmacd(sn)
         df1=(pd.concat([df1,df2],axis=1,join='outer')
                .fillna(0)
-           .pipe(compute)
+           .pipe(computeposmacd)
            .drop('posmacd',axis=1))
       
     #cols=['f1','f2','f3','f4']
     #df1[cols]=df1[cols].applymap(np.int64)    
     return df1.to_csv('SH8.csv')
 
-def compute(df):
+def computeposmacd(df):
     df.loc[df['posmacd']==1,'f1']=df.loc[df['posmacd']==1,'f1']+1
     df.loc[df['posmacd']==2,'f2']=df.loc[df['posmacd']==2,'f2']+1
     df.loc[df['posmacd']==3,'f3']=df.loc[df['posmacd']==3,'f3']+1
@@ -47,3 +47,47 @@ def savesh8w():
         df2=STS.getwposmacd(sn)
         df=df.append(df2)
     return df.to_csv('sh8w.txt')
+
+
+
+def gettmacddf(sn='ss123456'):
+    imp.reload(STS)
+    df1=(STS.gettmacd('ss123456')
+            .drop('tmacd',axis=1)
+            .assign(f1=0)
+            .assign(f2=0)
+            .loc['2015-1-1':])
+    a=STFILE.ANALYSIS()
+    for sn in a.getallfile('SH8'):
+        df2=STS.gettmacd(sn)
+        df1=(pd.concat([df1,df2],axis=1,join='outer')
+               .fillna(0)
+           .pipe(computetmacd)
+           .drop('tmacd',axis=1))
+      
+    #cols=['f1','f2','f3','f4']
+    #df1[cols]=df1[cols].applymap(np.int64)    
+    return df1.to_csv('SH8t.csv')
+
+def computetmacd(df):
+    df.loc[df['tmacd']==1,'f1']=df.loc[df['tmacd']==1,'f1']+1
+    df.loc[df['tmacd']==0,'f2']=df.loc[df['tmacd']==0,'f2']+1
+    return df
+
+def savesh8tmacdd():
+    imp.reload(STS)
+    a=STFILE.ANALYSIS()
+    df=pd.DataFrame()
+    for sn in a.getallfile('SH8'):
+        df2=STS.getdtmacd(sn)
+        df=df.append(df2)
+    return df.to_csv('sh8dt.txt')
+
+def savesh8tmacdw():
+    imp.reload(STS)
+    a=STFILE.ANALYSIS()
+    df=pd.DataFrame()
+    for sn in a.getallfile('SH8'):
+        df2=STS.getwtmacd(sn)
+        df=df.append(df2)
+    return df.to_csv('sh8wt.txt')
