6c994a2deb7a8d5c71ab8d436ac4721ef192d321
libopen <libin_chn@msn.com>
2017-12-11 10:48:08 +0800

    optimize ists

--- STS.py
+++ STS.py
@@ -2,72 +2,71 @@ import pandas as pd
 import STTB,imp
 import numpy as np
 
-def getposmacd(sn='ss123456'):
+def getposmacd(sn='ss123456',datatype='day'):
     imp.reload(STTB)
-    s=STTB.STDTB(sn)
-    exdb=(s.getexdb()[['date','posmacd']]
-           .set_index(['date'])
-           .loc['2015-1-1':]
-           )
-    return exdb
-
-
-def getdposmacd(sn='ss123456'):
-    imp.reload(STTB)
-    s=STTB.STDTB(sn)
-    exdb=(s.getexdb()[['date','posmacd']]
-           .set_index(['date'])
-           .loc['2015-1-1':]
-           .assign(sn=lambda x:sn)
-           )
-    return exdb
-
-def getwposmacd(sn='ss123456'):
-    imp.reload(STTB)
-    s=STTB.STWTB(sn)
+    if datatype=='day':
+        s=STTB.STDTB(sn)
+    else:  # by week
+        s=s=STTB.STWTB(sn)
     try:
         exdb=(s.getexdb()[['date','posmacd']]
                .set_index(['date'])
-               .loc['2015-1-1':]
-               .assign(sn=lambda x:sn)
+               .loc['2014-6-1':]
                )
         return exdb
     except:
         return None
 
-
-def gettmacd(sn='ss123456'):
+def GetPosmacdDetail(sn='ss123456',datatype='day'):
     imp.reload(STTB)
-    s=STTB.STDTB(sn)
-    exdb=(s.getexdb()[['date','tmacd']]
-           .set_index(['date'])
-           .loc['2015-1-1':]
-           )
-    return exdb
-
-def getdtmacd(sn='ss123456'):
-    imp.reload(STTB)
-    s=STTB.STDTB(sn)
+    if datatype=='day':
+        s=STTB.STDTB(sn)
+    else:  # by week
+        s=STTB.STWTB(sn)
     try:
-        exdb=(s.getexdb()[['date','tmacd']]
+        exdb=(s.getexdb()[['date','posmacd']]
                .set_index(['date'])
-               .loc['2015-1-1':]
+               .loc['2014-6-1':]
                .assign(sn=lambda x:sn)
                )
         return exdb
     except:
         return None
 
-def getwtmacd(sn='ss123456'):
-    imp.reload(STTB)
-    s=STTB.STWTB(sn)
-    try:
-        exdb=(s.getexdb()[['date','tmacd']]
-               .set_index(['date'])
-               .loc['2015-1-1':]
-               .assign(sn=lambda x:sn)
-               )
-        return exdb
-    except:
-        return None
+
+
+#def gettmacd(sn='ss123456'):
+    #imp.reload(STTB)
+    #s=STTB.STDTB(sn)
+    #exdb=(s.getexdb()[['date','tmacd']]
+           #.set_index(['date'])
+           #.loc['2015-1-1':]
+           #)
+    #return exdb
+
+#def getdtmacd(sn='ss123456'):
+    #imp.reload(STTB)
+    #s=STTB.STDTB(sn)
+    #try:
+        #exdb=(s.getexdb()[['date','tmacd']]
+               #.set_index(['date'])
+               #.loc['2015-1-1':]
+               #.assign(sn=lambda x:sn)
+               #)
+        #return exdb
+    #except:
+        #return None
+
+#def getwtmacd(sn='ss123456'):
+    #imp.reload(STTB)
+    #s=STTB.STWTB(sn)
+    #try:
+        #exdb=(s.getexdb()[['date','tmacd']]
+               #.set_index(['date'])
+               #.loc['2015-1-1':]
+               #.assign(sn=lambda x:sn)
+               #)
+        #return exdb
+    #except:
+        #return None
     
--- ists.py
+++ ists.py
@@ -2,7 +2,7 @@ import pandas as pd
 import STS,imp,STFILE
 import numpy as np
 
-def getposdf(sn='ss123456'):
+def getposdf(datatype='day'):
     imp.reload(STS)
     df1=(STS.getposmacd('ss123456')
             .drop('posmacd',axis=1)
@@ -10,18 +10,19 @@ def getposdf(sn='ss123456'):
             .assign(f2=0)
             .assign(f3=0)
             .assign(f4=0)
-            .loc['2015-1-1':])
+            .loc['2014-6-1':])
     a=STFILE.ANALYSIS()
     for sn in a.getallfile('SH8'):
-        df2=STS.getposmacd(sn)
-        df1=(pd.concat([df1,df2],axis=1,join='outer')
-               .fillna(0)
-           .pipe(computeposmacd)
-           .drop('posmacd',axis=1))
-      
+        df2=STS.getposmacd(sn,datatype)
+        if df2 is not None:
+            df1=(pd.concat([df1,df2],axis=1,join='outer')
+                   .fillna(0)
+               .pipe(computeposmacd)
+               .drop('posmacd',axis=1))
+          
     #cols=['f1','f2','f3','f4']
     #df1[cols]=df1[cols].applymap(np.int64)    
-    return df1.to_csv('SH8.csv')
+    return df1.to_csv("index_{}.csv".format(datatype))
 
 def computeposmacd(df):
     df.loc[df['posmacd']==1,'f1']=df.loc[df['posmacd']==1,'f1']+1
@@ -30,64 +31,75 @@ def computeposmacd(df):
     df.loc[df['posmacd']==4,'f4']=df.loc[df['posmacd']==4,'f4']+1
     return df
 
-def savesh8d():
+def saveIndexDetail(datatype='day'):
     imp.reload(STS)
     a=STFILE.ANALYSIS()
     df=pd.DataFrame()
     for sn in a.getallfile('SH8'):
-        df2=STS.getdposmacd(sn)
+        df2=STS.GetPosmacdDetail(sn,datatype)
         df=df.append(df2)
-    return df.to_csv('sh8d.txt')
-
-def savesh8w():
-    imp.reload(STS)
-    a=STFILE.ANALYSIS()
-    df=pd.DataFrame()
-    for sn in a.getallfile('SH8'):
-        df2=STS.getwposmacd(sn)
-        df=df.append(df2)
-    return df.to_csv('sh8w.txt')
+    return df.to_csv("indexDetail_".format(datatype))
 
 
 
-def gettmacddf(sn='ss123456'):
-    imp.reload(STS)
-    df1=(STS.gettmacd('ss123456')
-            .drop('tmacd',axis=1)
-            .assign(f1=0)
-            .assign(f2=0)
-            .loc['2015-1-1':])
-    a=STFILE.ANALYSIS()
-    for sn in a.getallfile('SH8'):
-        df2=STS.gettmacd(sn)
-        df1=(pd.concat([df1,df2],axis=1,join='outer')
-               .fillna(0)
-           .pipe(computetmacd)
-           .drop('tmacd',axis=1))
+
+
+#def gettmacddf():
+    #imp.reload(STS)
+    #df1=(STS.gettmacd('ss123456')
+            #.drop('tmacd',axis=1)
+            #.assign(f1=0)
+            #.assign(f2=0)
+            #.loc['2015-1-1':])
+    #a=STFILE.ANALYSIS()
+    #for sn in a.getallfile('SH8'):
+        #df2=STS.gettmacd(sn)
+        #df1=(pd.concat([df1,df2],axis=1,join='outer')
+               #.fillna(0)
+           #.pipe(computetmacd)
+           #.drop('tmacd',axis=1))
       
-    #cols=['f1','f2','f3','f4']
-    #df1[cols]=df1[cols].applymap(np.int64)    
-    return df1.to_csv('SH8t.csv')
+    ##cols=['f1','f2','f3','f4']
+    ##df1[cols]=df1[cols].applymap(np.int64)    
+    #return df1.to_csv('SH8t.csv')
 
-def computetmacd(df):
-    df.loc[df['tmacd']==1,'f1']=df.loc[df['tmacd']==1,'f1']+1
-    df.loc[df['tmacd']==0,'f2']=df.loc[df['tmacd']==0,'f2']+1
-    return df
+#def computetmacd(df):
+    #df.loc[df['tmacd']==1,'f1']=df.loc[df['tmacd']==1,'f1']+1
+    #df.loc[df['tmacd']==0,'f2']=df.loc[df['tmacd']==0,'f2']+1
+    #return df
 
-def savesh8tmacdd():
-    imp.reload(STS)
-    a=STFILE.ANALYSIS()
-    df=pd.DataFrame()
-    for sn in a.getallfile('SH8'):
-        df2=STS.getdtmacd(sn)
-        df=df.append(df2)
-    return df.to_csv('sh8dt.txt')
+#def savesh8tmacdd():
+    #imp.reload(STS)
+    #a=STFILE.ANALYSIS()
+    #df=pd.DataFrame()
+    #for sn in a.getallfile('SH8'):
+        #df2=STS.getdtmacd(sn)
+        #df=df.append(df2)
+    #return df.to_csv('sh8dt.txt')
 
-def savesh8tmacdw():
-    imp.reload(STS)
-    a=STFILE.ANALYSIS()
-    df=pd.DataFrame()
-    for sn in a.getallfile('SH8'):
-        df2=STS.getwtmacd(sn)
-        df=df.append(df2)
-    return df.to_csv('sh8wt.txt')
+#def savesh8tmacdw():
+    #imp.reload(STS)
+    #a=STFILE.ANALYSIS()
+    #df=pd.DataFrame()
+    #for sn in a.getallfile('SH8'):
+        #df2=STS.getwtmacd(sn)
+        #df=df.append(df2)
+    #return df.to_csv('sh8wt.txt')
+
+
+
+def getsh8():
+    getposdf('day')
+    getposdf('week')
+   
+    
+    
+
+
+def main():
+    getsh8()
+
+if __name__=="__main__":
+    main()
+
+    
